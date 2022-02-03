7f7783e9275c8ad212f178b11a01d814c38e567a
Steven Kristjan Rekand <s.rekand@khk.ee>
2022-02-03 12:33:22 +0200

    Created form request responce with API data

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const fetch = require('node-fetch')
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -27,4 +29,24 @@ app.get('/', function (req, res) {
     })
 })
 
-app.listen(3000)
+app.post('/', function(req,res){
+    console.log(req.body)
+    let city = req.body.cityname
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${
+        city}&appid=${key}`)
+        .then((responce) =>{
+            return responce.json()
+        })
+        .then((data) =>{
+            let description = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp)-273.15)
+            res.render('index', {
+                description: description,
+                city: city,
+                temp: temp
+            })
+        })
+})
+
+app.listen(1800)
